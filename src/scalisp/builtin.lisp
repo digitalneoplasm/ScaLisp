0922cdba2b371efe308fbd07ff0bc84155b31ebd
Daniel R. Schlegel <drschleg@buffalo.edu>
2019-05-31 15:22:05 -0400

    Some builtin functions.

--- src/scalisp/builtin.lisp
+++ src/scalisp/builtin.lisp
@@ -1,3 +1,22 @@
-(defun map (f lst) (if (atom lst) lst (cons (f (car lst)) (map f (cdr lst)))))
+(defun map (f lst)
+  (if (atom lst)
+    lst
+    (cons (f (car lst)) (map f (cdr lst)))))
 
-(defun ff (lst) (if (atom lst) lst (ff (car lst))))
+(defun ff (lst)
+  (if (atom lst)
+    lst
+    (ff (car lst))))
+
+;; car/cdr compound operations
+(defun caar (x)
+  (car (car x)))
+
+(defun cadr (x)
+  (car (cdr x)))
+
+(defun cdar (x)
+  (cdr (car x)))
+
+(defun cddr (x)
+  (cdr (cdr x)))
